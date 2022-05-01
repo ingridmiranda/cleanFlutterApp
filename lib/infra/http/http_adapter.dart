e95523c2c0a9531e188fe1175f300b704257ce9a
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-01 18:58:03 -0300

    ensure httpAdapter throws BadRequestError on 400

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/http/http.dart';
-import 'package:http/http.dart';
 
 class HttpAdapter implements HttpClient {
   final Client client;
@@ -14,26 +14,23 @@ class HttpAdapter implements HttpClient {
   Future<Map?> request(
       {required String url, required String method, Map? body}) async {
     Response? response;
-    try {
-      final headers = {
-        'content-type': 'application/json',
-        'accept': 'application/json'
-      };
-      final jsonBody = body != null ? jsonEncode(body) : null;
-      response =
-          await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-      return _handleResponse(response);
-    } catch (e) {
-      debugPrint(e.toString());
-      return null;
-    }
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    response =
+        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    return _handleResponse(response);
   }
 
   Map? _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isNotEmpty ? jsonDecode(response.body) : null;
-    } else {
+    } else if (response.statusCode == 204) {
       return null;
+    } else {
+      throw HttpError.badRequest;
     }
   }
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:cleanflutterapp/data/http/http.dart';
 import 'package:cleanflutterapp/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -80,6 +79,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
