d06c644976bc90ff7c92ec0f711e5f360211ed1f
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-01 19:10:21 -0300

    ensure httpAdapter throws ForbiddenError on 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -105,12 +105,20 @@ void main() {
       expect(future, throwsA(HttpError.serverError));
     });
 
-    test('Should return ServerError if post returns 401', () async {
+    test('Should return UnauthorizedError if post returns 401', () async {
       mockResponse(401);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return UnauthorizedError if post returns 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
     });
   });
 }
