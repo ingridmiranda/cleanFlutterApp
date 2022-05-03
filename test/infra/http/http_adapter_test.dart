17e7536d2e53d4604332e692acb776f2b49b2dcb
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-03 18:56:15 -0300

    fix ForbiddenError at test label

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -113,7 +113,7 @@ void main() {
       expect(future, throwsA(HttpError.unauthorized));
     });
 
-    test('Should return UnauthorizedError if post returns 403', () async {
+    test('Should return ForbiddenError if post returns 403', () async {
       mockResponse(403);
 
       final future = sut.request(url: url, method: 'post');
