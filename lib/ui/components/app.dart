f98a9fcc8d4bad964e7f1ba05b99bbd7bced6cd2
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-09 22:27:29 -0300

    add LoginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -7,7 +7,81 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: '4dev', debugShowCheckedModeBanner: false, home: LoginPage());
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
+    return MaterialApp(
+        title: '4dev',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: primaryColor,
+            primaryColorDark: primaryColorDark,
+            primaryColorLight: primaryColorLight,
+            colorScheme:
+                ColorScheme.fromSwatch().copyWith(secondary: primaryColor),
+            backgroundColor: Colors.white,
+            textTheme: const TextTheme(
+              headline1: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: primaryColorDark),
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColorLight)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColor)),
+              alignLabelWithHint: true,
+              floatingLabelStyle: TextStyle(color: primaryColorLight),
+            ),
+            textButtonTheme: TextButtonThemeData(
+                style: ButtonStyle(
+              shape: MaterialStateProperty.resolveWith((states) =>
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              foregroundColor: MaterialStateProperty.resolveWith((states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return primaryColorLight;
+                } else {
+                  return primaryColor;
+                }
+              }),
+              backgroundColor:
+                  MaterialStateProperty.resolveWith((states) => Colors.white),
+              padding: MaterialStateProperty.resolveWith(
+                (states) =>
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+            )),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ButtonStyle(
+              shape: MaterialStateProperty.resolveWith((states) =>
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              backgroundColor: MaterialStateProperty.resolveWith((states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return primaryColorLight;
+                } else {
+                  return primaryColor;
+                }
+              }),
+              foregroundColor:
+                  MaterialStateProperty.resolveWith((states) => Colors.white),
+              padding: MaterialStateProperty.resolveWith(
+                (states) =>
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+            )),
+            buttonTheme: ButtonThemeData(
+                colorScheme: const ColorScheme.light(primary: primaryColor),
+                buttonColor: primaryColor,
+                splashColor: primaryColorLight,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)))),
+        home: const LoginPage());
   }
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/component.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,30 +9,41 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Image(image: AssetImage('lib/ui/assets/logo.png')),
-            Text('Login'.toUpperCase()),
-            Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Email', icon: Icon(Icons.email)),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Senha', icon: Icon(Icons.lock)),
-                  obscureText: true,
-                ),
-                ElevatedButton(
-                    onPressed: () {}, child: Text('Entrar'.toUpperCase())),
-                TextButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.person),
-                    label: const Text('Criar Conta'))
-              ],
-            ))
+            const LoginHeader(),
+            const Headline1(text: 'Login'),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email,
+                            color: Theme.of(context).primaryColorLight)),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 32),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock,
+                              color: Theme.of(context).primaryColorLight)),
+                      obscureText: true,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text('Entrar'.toUpperCase())),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Criar Conta'))
+                ],
+              )),
+            )
           ],
         ),
       ),
