e88b92f8dd5fdf2d0d58ec58dc4641755a440e7e
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-15 15:56:46 -0300

    move SpinnerDialog to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -25,25 +25,9 @@ class _LoginPageState extends State<LoginPage> {
         builder: (context) {
           widget.presenter?.isLoadingStream?.listen((isLoading) {
             if (isLoading == true) {
-              showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) => SimpleDialog(
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              CircularProgressIndicator(),
-                              SizedBox(height: 10),
-                              Text('Aguarde...', textAlign: TextAlign.center)
-                            ],
-                          )
-                        ],
-                      ));
+              showLoading(context);
             } else {
-              if (Navigator.canPop(context)) {
-                Navigator.of(context).pop();
-              }
+              hideLoading(context);
             }
           });
 
