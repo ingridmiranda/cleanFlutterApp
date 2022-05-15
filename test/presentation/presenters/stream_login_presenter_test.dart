1ba795dfc7cd5f7a0ad129555e24ec8b99adab3f
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-15 18:36:10 -0300

    move sut creation to the setup method

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -7,26 +7,33 @@ abstract class Validation {
 }
 
 class StreamLoginPresenter {
-  final Validation validation;
-  StreamLoginPresenter({required this.validation});
+  final Validation? validation;
+  StreamLoginPresenter({this.validation});
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    validation?.validate(field: 'email', value: email);
   }
 }
 
 class ValidationSpy extends Mock implements Validation {}
 
 void main() {
+  StreamLoginPresenter? sut;
+  ValidationSpy? validation;
+  String? email;
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+  });
+
   test(
     'Should call Validation with correct email',
     () {
-      final validation = ValidationSpy();
-      final sut = StreamLoginPresenter(validation: validation);
-      final email = faker.internet.email();
+      sut?.validateEmail(email ?? "");
 
-      sut.validateEmail(email);
-
-      verify(() => validation.validate(field: 'email', value: email)).called(1);
+      verify(() => validation?.validate(field: 'email', value: email ?? ""))
+          .called(1);
     },
   );
 }
