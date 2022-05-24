02cf327a7a3742a15d58f6c039959471d8b2ccc0
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-05-23 22:31:38 -0300

    ensure StreamLoginPresenter emits password error as null if validation succeds

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -91,4 +91,17 @@ void main() {
       sut?.validatePassword(email ?? "");
     },
   );
+
+  test(
+    'Should emit null if validation succeeds',
+    () {
+      sut?.passwordErrorStream
+          .listen(expectAsync1((error) => expect(error, null)));
+      sut?.isFormValidStream
+          .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+      sut?.validatePassword(email ?? "");
+      sut?.validatePassword(email ?? "");
+    },
+  );
 }
